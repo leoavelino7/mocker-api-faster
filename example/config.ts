d5d85669610978da756fe7c9632c279c7db5973b
Leonardo Avelino <>
2022-04-13 09:23:15 -0300

    update: change example

--- example/config.ts
+++ example/config.ts
@@ -1,11 +1,24 @@
 import { Config } from "mocker-api-faster";
+import { join } from "path";
+import dotenv from "dotenv";
 
-const PORT = 8000;
+dotenv.config();
+
+const {
+  FIXTURE_PATH = "fixtures",
+  ROUTES_PATH = "routes",
+  API_PORT = 8000,
+  URL_INIT = "/",
+  URL_MOCKS_LIST = "/mocks",
+  APP_NAME = "API Mocker",
+} = process.env;
 
 export const config: Config = {
-  PORT,
-  URL_INIT: "/",
-  URL_MOCKS_LIST: "/mocks",
-  APP_NAME: "Mocker API",
-  LOGGER_INIT: `⚡️[server]: Server is running at https://localhost:${PORT}`,
+  FIXTURE_PATH: join(__dirname, FIXTURE_PATH),
+  ROUTES_PATH: join(__dirname, ROUTES_PATH),
+  PORT: Number(API_PORT),
+  URL_INIT,
+  URL_MOCKS_LIST,
+  APP_NAME,
+  LOGGER_INIT: `⚡️[server]: Server is running at https://localhost:${API_PORT}`,
 };
